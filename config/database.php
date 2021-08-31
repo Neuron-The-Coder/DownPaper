81a8b92cbfa4f58bf6938a7a3cce641779c02a5c
Doctor Neuron <darren.farell@binus.ac.id>
2021-08-31 19:59:00 +0700

    Update DB

--- config/database.php
+++ config/database.php
@@ -1,8 +1,10 @@
 <?php
 
 use Illuminate\Support\Str;
+$DATABASE_URL = parse_url("postgres://gfspdmelcafecp:dacb4daa09f33d832e2bcf80e256bfc25a821ee640c82e1abfade5a4eb1110bb@ec2-23-20-124-77.compute-1.amazonaws.com:5432/d97ddj53jhemer");
 
 return [
+
 
     /*
     |--------------------------------------------------------------------------
@@ -15,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +67,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => $DATABASE_URL["host"],
+            'host' => $DATABASE_URL["port"],
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
