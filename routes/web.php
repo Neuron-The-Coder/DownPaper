e000dfd62390412f1f2b1771df2d744758ed382c
Doctor Neuron <darren.farell@binus.ac.id>
2021-09-01 17:38:55 +0700

    Route1

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [WallpaperController::class, 'getViewWallpaper'])->name('dashboard');
+Route::get('/test', function() {
+  return 'This App working correctly, just the DB is f-ed up';
+});
