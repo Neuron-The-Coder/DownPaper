8cbc743af95487bc2a18f960497bd2003e120c32
Doctor Neuron <darren.farell@binus.ac.id>
2021-09-03 17:15:09 +0700

    a

---
+++ app/Providers/DropboxServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+ 
+namespace App\Providers;
+ 
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use Spatie\Dropbox\Client as DropboxClient;
+use Spatie\FlysystemDropbox\DropboxAdapter;
+ 
+class DropboxServiceProvider extends ServiceProvider
+{
+    public function register(){
+        
+    }
+    public function boot()
+    {
+        Storage::extend('dropbox', function ($app, $config) {
+            $client = new DropboxClient(
+                $config['authorization_token']
+            );
+  
+            return new Filesystem(new DropboxAdapter($client));
+        });
+    }
+}
